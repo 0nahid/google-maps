f7426ed5aae5fab1de48fc8d216dbc4f6037d932
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-04 22:26:49 +0600

    maps project started

---
+++ app.js
@@ -0,0 +1,9 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 2,
+        center: {
+            lat: 24.2513,
+            lng: -89.9167
+        },
+    });
+}
